f9cdc7ff83e55e30273294639c845a906891d935
Drew Swinney <drewswinney@gmail.com>
2025-12-17 15:41:23 -0500

    Deploy code for PA1

--- libs/polyflow/odrive_s1/node.launch.py
+++ libs/polyflow/odrive_s1/node.launch.py
@@ -8,7 +8,7 @@ def generate_launch_description():
     inbound_connections = json.loads('[]')
     outbound_connections = json.loads('[]')
     env = {
-        "POLYFLOW_NODE_ID": "69431366d1dce07ccaa72d4e",
+        "POLYFLOW_NODE_ID": "6943155ed1dce07ccaa72db5",
         "POLYFLOW_PARAMETERS": json.dumps(parameters),
         "POLYFLOW_CONFIGURATION": json.dumps(configuration),
         "POLYFLOW_INBOUND_CONNECTIONS": json.dumps(inbound_connections),
